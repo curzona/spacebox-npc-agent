d25638ab93ac352a00c172cfffdd8810402262a7
Jordan Curzon <curzonj@gmail.com>
2014-10-02 09:19:02 -0700

    the builder can build an ore mine from the starter package

--- builder.js
+++ builder.js
@@ -19,67 +19,12 @@ var game = {
     byAccount: {}
 };
 
-var gameAgentFSM = new machina.Fsm({
+var buildRequestFSM = new machina.Fsm({
     initialState: 'uninitialized',
-    eventListeners: {
-        nohandler: [ function(o) { console.log(o); } ],
-        handling: [ function(o) { console.log(o); } ],
-    },
-    states: {
-        'uninitialized': {
-            // TODO I don't actually know that I have nothing
-            'connected': 'have nothing',
-        },
-        'have nothing': {
-            _onEnter: function() {
-                cmd('spawnStarter');
-            },
-            'industry update': function(msg) {
-                if (msg.type == 'facility' && msg.blueprint == '7abb04d3-7d58-42d8-be93-89eb486a1c67' && msg.tombstone !== true) {
-                    this.starterShip = msg.uuid;
-                    this.handle('loadout complete');
-                } else {
-                    console.log('these are not the droids you are looking for');
-                
-                }
-            },
-            'loadout complete': 'have starter'
-        },
-        'have starter': {
-            _onEnter: function() {
-                this.handle('build a scaffold');
-            },
-            'build a scaffold': function() {
-                var self = this;
-                C.request('build', 'GET', 200, '/facilities?all=true').then(function(list) {
-                    console.log(list);
-                    return build('manufacture', 'ffb74468-7162-4bfb-8a0e-a8ae72ef2a8b', self.starterShip, 1);
-                }).then(function(body) {
-                    self.job_uuid = body.job.uuid;
-                }).done();
-            },
-            'industry update': function(msg) {
-                if (msg.type == 'job' && msg.uuid == this.job_uuid && msg.state == 'delivered') {
-                    this.handle('scaffold complete');
-                }
-            },
-            'scaffold complete': 'have a scaffold'
-        },
-        'have a scaffold': {
-            _onEnter: function() {
-                cmd('deploy', {
-                    shipID: this.starterShip,
-                    slice: 'default',
-                    blueprint: 'ffb74468-7162-4bfb-8a0e-a8ae72ef2a8b'
-                });
-            }
-        }
+    initialize: function() {
+
     }
 });
-
-setInterval(function() {
-    console.log("agent state: %s", gameAgentFSM.state);
-}, 1000);
 
 var websocketFSM = machina.Fsm.extend({
     initialState: 'uninitialized',
@@ -100,7 +45,9 @@ var websocketFSM = machina.Fsm.extend({
             'socket_connected': 'connected',
         },
         'connected': {
-            'json_message': function(m) { console.log(this.service_name, 'default', m); },
+            'json_message': function(msg) {
+                this.emit('message', msg);
+            },
             'socket_error': function(e) { console.log("error: %s", e); },
             'socket_message': function(m) { 
                 var data;
@@ -127,17 +74,6 @@ var websocketFSM = machina.Fsm.extend({
 var spaceWebsocketsFSM = new websocketFSM({
     service_name: 'space',
     states: {
-        'connected': {
-            'ready': function() {
-                gameAgentFSM.handle('connected');
-            },
-            'update': function(state) {
-                console.log(state.values);
-                gameAgentFSM.handle('space update', state);
-
-            },
-            'json_message': handleMessage,
-        },
         'disconnected': {
             _onEnter: function() {
                 // TODO we could reconnect, but I don't want to atm
@@ -151,14 +87,115 @@ var spaceWebsocketsFSM = new websocketFSM({
 
 var buildFSM = new websocketFSM({
     service_name: 'build',
-    states: {
-        'connected': {
-            'json_message': function(msg) {
-                gameAgentFSM.handle('industry update', msg);
+});
+
+var gameAgentFSM = new machina.Fsm({
+    initialState: 'uninitialized',
+    initialize: function() {
+        buildFSM.on('message', function(m) {
+            this.handle('industry update', m);
+        }.bind(this));
+        spaceWebsocketsFSM.on('message', function(m) {
+            if (m.type == 'state') {
+                this.handle('space update', m.state);
             }
+        }.bind(this));
+    },
+    eventListeners: {
+        nohandler: [ function(o) { console.log('nohandler', o); } ],
+        handling: [ function(o) { console.log('handling', o); } ],
+        transition: [ function(o) { console.log('transition', o); } ],
+    },
+    states: {
+        'uninitialized': {
+            // TODO I don't actually know that I have nothing
+            'connected': 'have nothing',
+        },
+        'have nothing': {
+            _onEnter: function() {
+                cmd('spawnStarter');
+            },
+            'industry update': function(msg) {
+                if (msg.type == 'facility' && msg.blueprint == '7abb04d3-7d58-42d8-be93-89eb486a1c67' && msg.tombstone !== true) {
+                    this.starterShip = msg.uuid;
+                    this.handle('loadout complete');
+                }
+            },
+            'loadout complete': 'have starter'
+        },
+        'have starter': {
+            _onEnter: function() {
+                this.handle('build a scaffold');
+            },
+            'build a scaffold': function() {
+                build('manufacture', 'ffb74468-7162-4bfb-8a0e-a8ae72ef2a8b',
+                      this.starterShip, 1).then(function(body) {
+                    this.job_uuid = body.job.uuid;
+                }.bind(this)).done();
+            },
+            'industry update': function(msg) {
+                if (msg.type == 'job' && msg.uuid == this.job_uuid && msg.state == 'delivered') {
+                    this.handle('scaffold complete');
+                }
+            },
+            'scaffold complete': 'have a scaffold'
+        },
+        'have a scaffold': {
+            _onEnter: function() {
+                cmd('deploy', {
+                    shipID: this.starterShip,
+                    slice: 'default',
+                    blueprint: 'ffb74468-7162-4bfb-8a0e-a8ae72ef2a8b'
+                });
+            },
+            'industry update': function(msg) {
+                if (msg.type == 'facility' && msg.blueprint == 'ffb74468-7162-4bfb-8a0e-a8ae72ef2a8b' && msg.tombstone !== true) {
+                    this.scaffold = msg.uuid;
+                    this.handle('deploy complete');
+                }
+            },
+            'deploy complete': 'scaffold deployed',
+        },
+        'scaffold deployed': {
+            _onEnter: function() {
+                var self = this;
+                C.request('inventory', 'POST', 204, '/inventory', [
+                    {
+                        inventory: this.starterShip,
+                        slice: 'default',
+                        blueprint: "f9e7e6b4-d5dc-4136-a445-d3adffc23bc6", // metal
+                        quantity: 2
+                    },
+                    {
+                        inventory: this.scaffold,
+                        slice: 'default',
+                        blueprint: "f9e7e6b4-d5dc-4136-a445-d3adffc23bc6", // metal
+                        quantity: 2
+                    },
+                ]).then(function() {
+                    return build('construct', "33e24278-4d46-4146-946e-58a449d5afae",
+                          self.scaffold, 1).then(function(body) {
+                        self.job_uuid = body.job.uuid;
+                    });
+                }).done();
+            },
+            'industry update': function(msg) {
+                if (msg.type == 'facility' && msg.blueprint == "33e24278-4d46-4146-946e-58a449d5afae" && msg.tombstone !== true) {
+                    this.handle('job complete');
+                }
+            },
+            'job complete': 'have a mine',
+        },
+        'have a mine': {
+
         }
     }
 });
+
+setInterval(function() {
+    console.log("agent state: %s", gameAgentFSM.state);
+}, 1000);
+
 
 function openWebSocket(which, fsm) {
     if (fsm === undefined) {
@@ -205,50 +242,49 @@ function openWebSocket(which, fsm) {
     });
 }
 
-function handleMessage(data) {
+spaceWebsocketsFSM.on('message', function(data) {
     switch (data.type) {
         case "arenaAccount":
             clientAuth = data.account;
-            break;
+        break;
         case "connectionReady":
-            spaceWebsocketsFSM.handle('ready');
-            break;
+            gameAgentFSM.handle('connected');
+        break;
         case "state":
             game.world[data.state.key] = deepMerge(data.state.values, game.world[data.state.key]);
 
-            if (game.world[data.state.key].type === 'spaceship') {
-                if (game.world[data.state.key].tombstone === true) {
-                    var data_account = game.world[data.state.key].account;
+        if (game.world[data.state.key].type === 'spaceship') {
+            if (game.world[data.state.key].tombstone === true) {
+                var data_account = game.world[data.state.key].account;
 
-                    if (data_account !== undefined && game.byAccount[data_account] !== undefined) {
-                        var i = game.byAccount[data_account].indexOf(data.state.key);
+                if (data_account !== undefined && game.byAccount[data_account] !== undefined) {
+                    var i = game.byAccount[data_account].indexOf(data.state.key);
 
-                        if (i > -1) {
-                            game.byAccount[data_account].splice(i, 1);
-                        }
-                    } else {
-                        console.log("unable to remove from account list " + data.state.key);
-                        //console.log(game.world[data.state.key]);
-                        //console.log(Object.keys(game.byAccount));
+                    if (i > -1) {
+                        game.byAccount[data_account].splice(i, 1);
                     }
+                } else {
+                    console.log("unable to remove from account list " + data.state.key);
+                    //console.log(game.world[data.state.key]);
+                    //console.log(Object.keys(game.byAccount));
+                }
 
-                    delete game.world[data.state.key];
-                } else if (data.state.values.account !== undefined) {
-                    if (game.byAccount[data.state.values.account] === undefined) {
-                        game.byAccount[data.state.values.account] = [];
-                    }
+                delete game.world[data.state.key];
+            } else if (data.state.values.account !== undefined) {
+                if (game.byAccount[data.state.values.account] === undefined) {
+                    game.byAccount[data.state.values.account] = [];
+                }
 
-                    if (game.byAccount[data.state.values.account].indexOf(data.state.key) == -1) {
-                        game.byAccount[data.state.values.account].push(data.state.key);
-                    }
+                if (game.byAccount[data.state.values.account].indexOf(data.state.key) == -1) {
+                    game.byAccount[data.state.values.account].push(data.state.key);
                 }
             }
+        }
 
-            spaceWebsocketsFSM.handle('update', data.state);
-            break;
+        break;
     }
 
-}
+});
 
 function cmd(name, opts) {
     if (opts === undefined) {
